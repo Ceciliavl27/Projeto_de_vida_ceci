74e7a541a2f8812ba6f9c98c062fd1d30deaeec1
Ceciliavl27 <cecilia.lipovieski@escola.pr.gov.br>
2026-05-28 15:19:03 +0000

    aula 04

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+
+const tempoObjetivo1 = new Date("2026-12-20T00:00:00");
+const tempoObjetivo2 = new Date("2026-11-15T00:00:00");
+const tempoObjetivo3 = new Date("2026-08-10T00:00:00");
+const tempoObjetivo4 = new Date("2026-09-05T00:00:00");
+
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[1].textContent = tempoObjetivo2 - tempoAtual;
+contadores[2].textContent = tempoObjetivo3 - tempoAtual;
+contadores[3].textContent = tempoObjetivo4 - tempoAtual;
 
 for (let i = 0; i < botoes.length; i++) {
 botoes[i].onclick = function () {
