6752b0a4e3f7db875f17f29a0989ea6e92410025
Ceciliavl27 <cecilia.lipovieski@escola.pr.gov.br>
2026-06-17 14:07:39 +0000

    aula 05

--- main.js
+++ main.js
@@ -3,16 +3,39 @@ const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 
 const tempoObjetivo1 = new Date("2026-12-20T00:00:00");
-const tempoObjetivo2 = new Date("2026-11-15T00:00:00");
-const tempoObjetivo3 = new Date("2026-08-10T00:00:00");
-const tempoObjetivo4 = new Date("2026-09-05T00:00:00");
+const tempoObjetivo2 = new Date("2026-07-15T00:00:00");
+const tempoObjetivo3 = new Date("2026-08-27T00:00:00");
+const tempoObjetivo4 = new Date("2026-06-14T00:00:00");
 
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
+
+function calculaTempo(tempoObjetivo) {
 let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
-contadores[1].textContent = tempoObjetivo2 - tempoAtual;
-contadores[2].textContent = tempoObjetivo3 - tempoAtual;
-contadores[3].textContent = tempoObjetivo4 - tempoAtual;
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
+return (
+dias +
+" dias " +
+horas +
+" horas " +
+minutos +
+" minutos " +
+segundos +
+" segundos"
+);
+}
 
 for (let i = 0; i < botoes.length; i++) {
 botoes[i].onclick = function () {
