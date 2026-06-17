1f06b1e41a1c3c15941876ab2862736dd5f18f87
Ceciliavl27 <cecilia.lipovieski@escola.pr.gov.br>
2026-06-17 14:13:31 +0000

    aula 06

--- main.js
+++ main.js
@@ -7,10 +7,24 @@ const tempoObjetivo2 = new Date("2026-07-15T00:00:00");
 const tempoObjetivo3 = new Date("2026-08-27T00:00:00");
 const tempoObjetivo4 = new Date("2026-06-14T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-contadores[1].textContent = calculaTempo(tempoObjetivo2);
-contadores[2].textContent = calculaTempo(tempoObjetivo3);
-contadores[3].textContent = calculaTempo(tempoObjetivo4);
+const tempos = [
+tempoObjetivo1,
+tempoObjetivo2,
+tempoObjetivo3,
+tempoObjetivo4
+];
+
+for (let i = 0; i < botoes.length; i++) {
+botoes[i].onclick = function () {
+for (let j = 0; j < botoes.length; j++) {
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+}
+
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+};
+}
 
 function calculaTempo(tempoObjetivo) {
 let tempoAtual = new Date();
@@ -25,6 +39,7 @@ segundos %= 60;
 minutos %= 60;
 horas %= 24;
 
+if (tempoFinal > 0) {
 return (
 dias +
 " dias " +
@@ -35,16 +50,20 @@ minutos +
 segundos +
 " segundos"
 );
+} else {
+return "Prazo Finalizado";
+}
 }
 
-for (let i = 0; i < botoes.length; i++) {
-botoes[i].onclick = function () {
-for (let j = 0; j < botoes.length; j++) {
-botoes[j].classList.remove("ativo");
-textos[j].classList.remove("ativo");
+function atualizaCronometro() {
+for (let i = 0; i < contadores.length; i++) {
+contadores[i].textContent = calculaTempo(tempos[i]);
+}
 }
 
-botoes[i].classList.add("ativo");
-textos[i].classList.add("ativo");
-};
+function comecaCronometro() {
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
 }
+
+comecaCronometro();
